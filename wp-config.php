c221596c501fe9dae28cb0dbb2d8708a456a0a2a
Felipe Ortiz Arredondo <107318069+felipeortizarredondo@users.noreply.github.com>
2023-07-03 22:32:56 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'nombre_de_la_base_de_datos');
-define('DB_USER', 'nombre_de_usuario');
-define('DB_PASSWORD', 'contraseña');
-define('DB_HOST', 'nombre_de_host_de_RDS');
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpressuser');
+define('DB_PASSWORD', 'wordpresspassword');
+define('DB_HOST', 'endpoint_de_RDS');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
